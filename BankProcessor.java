f105c66d1a611052a107dfdbf271c5b2b2136e3a
koppoju vinay <vinaykoppoju058@gmail.com>
2026-07-26 10:42:47 +0530

    initial commit

---
+++ BankProcessor.java
@@ -0,0 +1,30 @@
+package com.constructot;
+import java.util.Scanner;
+import com.constructot.*;
+
+public class BankProcessor {
+		public static void main(String[] args) {
+			Scanner sc = new Scanner(System.in);
+			System.out.println("Account number:");
+			int number=sc.nextInt();
+			sc.nextLine();
+			System.out.println("account holder name:");
+			String name = sc.nextLine();
+			System.out.println("Account balance:");
+			double balance= sc.nextDouble();
+			bankAccount obj = new bankAccount(number,name,balance);
+			
+			
+			
+			System.out.println("depost Amount:");
+			double deposit = sc.nextDouble();
+			obj.deposit(deposit);
+			System.out.println("Amount withdrawl:");
+			double withdrawl = sc.nextDouble();
+			obj.withdraw(withdrawl);
+			
+			
+		}
+	}
+
+
